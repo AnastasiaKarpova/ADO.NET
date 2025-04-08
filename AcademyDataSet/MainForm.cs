114ef743e57841298411ab90d57af42169ea2080
Anastasia <karpovanastiya@mail.ru>
2025-04-08 21:54:08 +0300

    Parent detection DONE

--- AcademyDataSet/MainForm.cs
+++ AcademyDataSet/MainForm.cs
@@ -61,7 +61,7 @@ namespace AcademyDataSet
 			string[] tables = this.tables.ToArray();
 			for(int i = 0; i < tables.Length; i++)
 			{
-				string cmd = $"SELECT * FROM {tables[i].Split(',')[0]}";
+				string cmd = $"SELECT * FROM {tables[i].Split(',')[0]}"; //???
 				SqlDataAdapter adapter = new SqlDataAdapter(cmd, connection);
 				adapter.Fill(GroupsRelatedData.Tables[tables[i].Split(',')[0]]);
 			}
@@ -127,35 +127,74 @@ namespace AcademyDataSet
 		{
 			Console.WriteLine("\n-----------------------------------\n");
 			Console.WriteLine(hasParents(table));
+			string relation_name = "No relation";
+			string parent_table_name = "";
+			string parent_column_name = "";
+			int parent_index = -1;
+			if (hasParents(table))
+			{
+				//relation_name = GroupsRelatedData.Tables[table].ParentRelations[0].RelationName.Substring(table.Length); 
+				relation_name = GroupsRelatedData.Tables[table].ParentRelations[0].RelationName; 
+				parent_table_name = GroupsRelatedData.Tables[table].ParentRelations[0].ParentTable.TableName; 
+				parent_column_name = parent_table_name.ToLower().Substring(0,parent_table_name.Length-1) + "_name"; 
+				Console.WriteLine(parent_table_name);
+				//DataColumn parent_column = GroupsRelatedData.Tables[parent_table_name].Columns["direction_name"];
+				//Console.WriteLine(parent_column.GetType());
+				parent_index = 
+					GroupsRelatedData.Tables[table].Columns.
+					IndexOf(parent_table_name.ToLower().Substring(0,parent_table_name.Length-1));
+				Console.WriteLine(parent_index);
+			}
 			foreach(DataRow row in GroupsRelatedData.Tables[table].Rows)
 			{
 				for(int i = 0; i < row.ItemArray.Length; i++)
 				{
-					Console.Write(row[i].ToString() + "\t");
+					if (i == parent_index)
+					{
+						DataRow parent_row = row.GetParentRow(relation_name);
+						Console.WriteLine(parent_row[parent_column_name]);
+						//Console.WriteLine(row.GetParentRow(relation_name)[parent_column_name]); 
+						//GroupsRelatedData.Tables;
+					}
+					else
+						Console.Write(row[i].ToString() + "\t");
+					//Console.WriteLine(row[i].GetType());
 				}
+				//if (hasParents(table))
+				//{
+				//	DataRow parent_row = row.GetParentRow(GroupsRelatedData.Tables[table].ParentRelations[0].RelationName);
+				//	//for (int j = 0; j < parent_row.ItemArray.Length; j++)
+				//	//	Console.Write(parent_row[j] + "\t");
+				//	Console.WriteLine(parent_row["direction_name"]);
+
+				//}
 				Console.WriteLine();
 			}
 			Console.WriteLine("\n-----------------------------------\n");
 		}
 		bool hasParents(string table)
 		{
+			return GroupsRelatedData.Tables[table].ParentRelations.Count > 0;
 			//bool yes = GroupsRelatedData.Relations.Contains(table);
 			//Console.WriteLine(yes);
 			//Console.WriteLine(GroupsRelatedData.Relations.ToString());
-			for(int i = 0; i < GroupsRelatedData.Relations.Count; i++)
-			{
-				if (GroupsRelatedData.Relations[i].ChildTable.TableName == table) return true;
-			}
-			return false;
+			//for(int i = 0; i < GroupsRelatedData.Relations.Count; i++)
+			//{
+			//	if (GroupsRelatedData.Relations[i].ChildTable.TableName == table) return true;
+			//}
+			//return false;
 		}
 		void Check()
 		{
 			AddTable("Directions", "direction_id,direction_name");
 			AddTable("Groups", "group_id,group_name,direction");
+			AddTable("Students","stud_id,last_name,first_name,middle_name,birth_date,group"); //никогда не брать название полей в квадратные скобки
 			AddRelation("GroupsDirections", "Groups,direction","Directions,direction_id");
+			AddRelation("StudentsGroups", "Students,group","Groups,group_id");
 			Load();
 			Print("Directions");
 			Print("Groups");
+			Print("Students");
 		}
 		[DllImport("kernel32.dll")]
 		public static extern bool AllocConsole();
