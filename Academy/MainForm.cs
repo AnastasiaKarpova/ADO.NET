085f47075e4d126b5968cd5c60fe9abadee14601
Anastasia <karpovanastiya@mail.ru>
2025-04-17 20:36:02 +0300

    Parent Combo-Dependencies DONE

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -19,7 +19,8 @@ namespace Academy
 		public Dictionary<string, int> d_directions;
 		public Dictionary<string, int> d_groups;
 
-		public Dictionary<ComboBox, List<ComboBox>> d_dependencies;
+		public Dictionary<ComboBox, List<ComboBox>> d_children;
+		public Dictionary<ComboBox, List<ComboBox>> d_parents;
 
 		DataGridView[] tables;
 
@@ -56,9 +57,13 @@ namespace Academy
 		{
 			InitializeComponent();
 
-			d_dependencies = new Dictionary<ComboBox, List<ComboBox>>()
+			d_children = new Dictionary<ComboBox, List<ComboBox>>()
 			{
 				{cbStudentsDirection, new List<ComboBox>(){ cbStudentsGroup}}
+			};
+			d_parents = new Dictionary<ComboBox, List<ComboBox>>()
+			{
+				{cbStudentsGroup, new List<ComboBox> { cbStudentsDirection} }
 			};
 
 			tables = new DataGridView[]
@@ -112,35 +117,37 @@ namespace Academy
 			//Query query = queries[i];
 			//tables[i].DataSource = connector.Select(query.Columns, query.Tables, query.Condition, query.Group_by);
 			//toolStripStatusLabelCount.Text = status_messages[i] + CountRecordsInDGV(tables[i]);
+			#region SWITCH
 			/*switch(tabControl.SelectedIndex)
-			{
-				case 0:
-					dgvStudents.DataSource = connector.Select("last_name,first_name,middle_name,birth_date,group_name,direction_name", "Students,Groups,Directions","[group]=group_id AND direction=direction_id");
-					toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
-					break;
-				case 1:
-					dgvGroups.DataSource = connector.Select("group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,start_time,direction_name", "Groups,Directions","direction=direction_id");
-					toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
-					break;
-				case 2:
-					dgvDirections.DataSource = connector.Select
-						(
-						"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп', COUNT(stud_id) AS N'Количество студентов'", 
-						"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)", 
-						"", 
-						"direction_name"
-						);
-					toolStripStatusLabelCount.Text = $"Количество направлений:{dgvDirections.RowCount - 1}.";
-					break;
-				case 3:
-					dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
-					toolStripStatusLabelCount.Text = $"Количество дисциплин:{dgvDisciplines.RowCount - 1}.";
-					break;
-				case 4:
-					dgvTeachers.DataSource = connector.Select("*", "Teachers");
-					toolStripStatusLabelCount.Text = $"Количество преподавателей:{dgvTeachers.RowCount - 1}.";
-					break;
-			}*/
+				{
+					case 0:
+						dgvStudents.DataSource = connector.Select("last_name,first_name,middle_name,birth_date,group_name,direction_name", "Students,Groups,Directions","[group]=group_id AND direction=direction_id");
+						toolStripStatusLabelCount.Text = $"Количество студентов:{dgvStudents.RowCount - 1}.";
+						break;
+					case 1:
+						dgvGroups.DataSource = connector.Select("group_name,dbo.GetLearningDaysFor(group_name) AS weekdays,start_time,direction_name", "Groups,Directions","direction=direction_id");
+						toolStripStatusLabelCount.Text = $"Количество групп:{dgvGroups.RowCount - 1}.";
+						break;
+					case 2:
+						dgvDirections.DataSource = connector.Select
+							(
+							"direction_name,COUNT(DISTINCT group_id) AS N'Количество групп', COUNT(stud_id) AS N'Количество студентов'", 
+							"Students RIGHT JOIN Groups ON([group]=group_id) RIGHT JOIN Directions ON(direction=direction_id)", 
+							"", 
+							"direction_name"
+							);
+						toolStripStatusLabelCount.Text = $"Количество направлений:{dgvDirections.RowCount - 1}.";
+						break;
+					case 3:
+						dgvDisciplines.DataSource = connector.Select("*", "Disciplines");
+						toolStripStatusLabelCount.Text = $"Количество дисциплин:{dgvDisciplines.RowCount - 1}.";
+						break;
+					case 4:
+						dgvTeachers.DataSource = connector.Select("*", "Teachers");
+						toolStripStatusLabelCount.Text = $"Количество преподавателей:{dgvTeachers.RowCount - 1}.";
+						break;
+				}*/ 
+			#endregion
 		}
 
 		//private void cbDirection_SelectedIndexChanged(object sender, EventArgs e)
@@ -180,9 +187,9 @@ namespace Academy
 				("group_id,group_name", "Groups", i == 0 ? "" : $"[{cb_suffix.ToLower()}]={dictionary[(sender as ComboBox).SelectedItem.ToString()]}");
 			cbStudentsGroup.Items.Clear();
 			cbStudentsGroup.Items.AddRange(d_groups.Select(g => g.Key).ToArray());*/
-			if(d_dependencies.ContainsKey(sender as ComboBox))
+			if(d_children.ContainsKey(sender as ComboBox))
 			{
-				foreach(ComboBox cb in d_dependencies[sender as ComboBox])
+				foreach(ComboBox cb in d_children[sender as ComboBox])
 				{
 					GetDependentData(cb, sender as ComboBox);
 				}
@@ -199,8 +206,25 @@ namespace Academy
 			//	);
 			Query query = new Query(queries[tabControl.SelectedIndex]);
 			string condition = (i == 0 || (sender as ComboBox).SelectedItem == null ? "" : $"[{cb_suffix.ToLower()}]={dictionary[$"{(sender as ComboBox).SelectedItem}"]}");
+			string parent_condition = "";
+			if(d_parents.ContainsKey(sender as ComboBox))
+			{
+				foreach(ComboBox cb in d_parents[sender as ComboBox])
+				{
+					if (cb.SelectedItem != null && cb.SelectedIndex > 0)
+					{
+						string column_name = cb.Name.Substring(Array.FindLastIndex<char>(cb.Name.ToCharArray(), Char.IsUpper));
+						string parent_dictionary_name = $"d_{column_name.ToLower()}s";
+						Dictionary<string, int> parent_dictionary = this.GetType().GetField(parent_dictionary_name).GetValue(this) as Dictionary<string, int>;
+						if (parent_condition != "") parent_condition += " AND ";
+						parent_condition += $"[{column_name}] = {parent_dictionary[cb.SelectedItem.ToString()]}"; 
+					}
+				}
+			}
 			if (query.Condition == "") query.Condition = condition;
 			else if (condition != "") query.Condition += $" AND {condition}";
+			if (query.Condition == "") query.Condition = parent_condition;
+			else if (parent_condition != "") query.Condition += $" AND {parent_condition}";
 			LoadPage(tabControl.SelectedIndex, query);
 		}
 		void GetDependentData(ComboBox dependent, ComboBox determinant)
